ea9f32699dca8d59f694645403d1c243a4fe8bd5
Malcolm Diller <malcolm@dillerm.io>
2021-06-02 17:42:07 -0700

    rearranged initial script to be just the one script

--- src/powershell/script.ps1
+++ src/powershell/script.ps1
@@ -6,124 +6,6 @@ function PrintInfo([string] $msg)  {
 	[Console]::Error.WriteLine($msg)
 }
 
-#
-# SECTION 1: STRINGS
-# 
-
-PrintInfo "SECTION 1: STRINGS"
-
-$name = "Bob"
-$lastName = "Bobson"
-
-$name
-# PRINTS: Bob
-
-"The name is $lastName, $name $lastName"
-# PRINTS: The name is Bobson, Bob Bobson
-
-"I said, my name is $($name.ToUpper())!"
-# PRINTS: I said, my name is BOB!
-
-PrintInfo "escaping"
-$thingsBobHates = "There are 2 things I hate:`n- lollygagging`n- being `"tired`""
-$thingsBobHates
-# PRINTS:
-# There are 2 things I hate:
-#	- lollygagging
-#	- being "tired"
-
-
-PrintInfo "multi-line string"
-# this would print the same thing
-$thingsBobHates2 = @"
-There are 2 things I hate:
-- lollygagging
-- being "tired"
-"@
-
-
-PrintInfo "string list splitting/joining"
-$sentance = @( "An", "example", "sentance", "goes", "something", "like", "this" ) -join " "
-
-$sentance
-# PRINTS: An example sentance goes something like this
-
-$sentance -split " "
-# PRINTS: 
-# An
-# example
-# sentance
-# goes
-# something
-# like
-# this
-
-PrintInfo "regex"
-# this is some sloppy regex for a word that ends in s.
-$pattern = "(?<=\s)([^\s])([^\s]+s)(?= |$)"
-
-$sentance -replace $pattern,"banana"
-# PRINTS: An example sentance banana something like banana
-
-$match = [regex]::Match($sentance, $pattern)
-
-if ($match.Success) {
-	$match.groups[0].Value
-	# PRINTS: goes
-
-	$match.groups[1].Value
-	# PRINTS: g
-
-	$match.groups[2].Value
-	# PRINTS: oes
-}
-
-
-#
-# SECTION 2: LISTS
-# 
-
-PrintInfo "SECTION 2: LISTS"
-
-$numbers = @( 7, 123, 555 )
-
-$numbers += 12345
-
-PrintInfo "enumerating"
-foreach ($item in $numbers) {
-	$item
-}
-for ($i = 0; $i -lt $numbers.Length; $i++) {
-	$numbers[$i]
-}
-$numbers
-# (ALL OF THE ABOVE) PRINT:
-# 7
-# 123
-# 555
-# 12345
-
-PrintInfo "filtering"
-$numbers | ? { $_ -gt 200 }
-# PRINTS:
-# 555
-# 12345
-
-PrintInfo "mapping"
-$numbers | % { $_ + 200 }
-# map
-# 207
-# 323
-# 755
-# 12545
-
-
-#
-# SECTION 3: FILE SYSTEM
-# 
-
-PrintInfo "SECTION 3: FILE SYSTEM"
-
 # get the directory of this script
 $scriptDir = $PSScriptRoot
 
@@ -141,7 +23,7 @@ $files = $files | ? { -not $_.PSIsContainer }
 $files = $files | ? { $_.Extension -eq ".json" }
 
 # list all json files in the directory by full path name, then filename, then ext
-PrintInfo "json files:"
+PrintInfo "JSON files:"
 foreach ($file in $files) {
 	if (-not (Test-Path -Path $file.FullName)) {
 		# check if file exists, if it doesnt, just exit (shouldn't ever happen)
@@ -155,7 +37,6 @@ foreach ($file in $files) {
 	$file.BaseName
 	$file.Extension
 }
-
 
 # use the first json file
 $filename = $files[0].FullName
